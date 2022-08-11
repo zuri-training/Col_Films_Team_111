6b2595982563412f1a8ae0c491d0a5422f1444ed
Achigyus <gyuseachi@yahoo.com>
2022-08-11 03:25:43 +0100

    finished videoModal page, added hamburger functionality to library page

--- library.js
+++ library.js
@@ -8,6 +8,24 @@ const slide1 = document.querySelector(".slide1");
 const prevButton1 = document.getElementById("slide-arrow-prev1");
 const nextButton1 = document.getElementById("slide-arrow-next1");
 
+let libraryDrop = document.getElementById('libraryDrop')
+libraryDrop.addEventListener('click', () => {
+    let mobileBar = document.querySelector('.mobileBar')
+    mobileBar.classList.toggle('active')
+})
+
+let dropdown = document.getElementById("dropdown")
+dropdown.addEventListener('click', () => {
+    let sideBarHeadingListP = document.querySelectorAll('.sideBarHeadingListP')
+    let sideBar = document.querySelector('.sideBar')
+    sideBar.classList.toggle('active')
+    let moviesSection = document.querySelector('.moviesSection')
+    moviesSection.classList.toggle('active')
+    sideBarHeadingListP.forEach((item) => {
+        item.classList.toggle('active')
+    })
+})
+
 nextButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft += slideWidth;
